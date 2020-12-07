90597992e620717b0463c2d0cf45c7121946e792
Rangga <leorangga30@ymail.com>
2020-12-07 15:24:47 +0700

    initial commit

---
+++ muslim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  muslim
+//
+//  Created by Rangga Leo on 07/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
